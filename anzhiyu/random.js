42134c4f8b198e28d5bbfdf83f0aa66ec6a86f0a
qiushibio <qiushibio@outlook.com>
2024-08-18 20:14:40 +0800

    Site updated: 2024-08-18 20:14:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/17/LatexAndMarkdown/","2024/01/31/Analytical_chemistry_YuYue/","2024/01/29/hello-world/","2024/01/31/Biochemistry_Zhanghaonan/","2024/01/29/tutorial/","2024/01/31/Maojiongran/","2024/01/31/Molecular_Biology_Sunpanyi/","2024/01/31/Ecology_ShiZhaoqi/"];function toRandomPost(){
+var posts=["2024/01/31/Biochemistry_Zhanghaonan/","2024/01/31/Ecology_ShiZhaoqi/","2024/01/31/Analytical_chemistry_YuYue/","2024/03/17/LatexAndMarkdown/","2024/01/31/Maojiongran/","2024/01/31/Molecular_Biology_Sunpanyi/","2024/01/29/hello-world/","2024/01/29/tutorial/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
