7c5454188e9f6bd05fa0a689ebe9af671dbcd8bb
qiushibio <qiushibio@outlook.com>
2024-08-19 09:38:23 +0800

    Site updated: 2024-08-19 09:38:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/31/Analytical_chemistry_YuYue/","2024/01/31/Biochemistry_Zhanghaonan/","2024/01/31/Ecology_ShiZhaoqi/","2024/03/17/LatexAndMarkdown/","2024/01/31/Maojiongran/","2024/01/31/Molecular_Biology_Sunpanyi/","2024/08/19/feiyue1/","2024/08/19/feiyue2/","2024/08/19/feiyuepost/","2024/01/29/hello-world/","2024/01/29/tutorial/"];function toRandomPost(){
+var posts=["2024/01/31/Analytical_chemistry_YuYue/","2024/01/31/Biochemistry_Zhanghaonan/","2024/01/31/Ecology_ShiZhaoqi/","2024/01/31/Maojiongran/","2024/01/31/Molecular_Biology_Sunpanyi/","2024/03/17/LatexAndMarkdown/","2024/08/19/feiyue2/","2024/08/19/feiyue1/","2024/08/19/feiyuepost/","2024/01/29/hello-world/","2024/01/29/tutorial/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
